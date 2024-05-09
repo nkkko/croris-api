cd5ecc142ffacd142bb0c83e734108e099d2c3e7
nkkko <nikola.balic@gmail.com>
2024-05-09 22:41:59 +0200

    memory error solved

--- crosbi2.py
+++ crosbi2.py
@@ -21,7 +21,7 @@ def fetch_publications(url, headers):
 
 def save_publications(publications, output_file):
     try:
-        with open(output_file, "wb") as file:
+        with open(output_file, "ab") as file:
             pickle.dump(publications, file)
         logging.info(f"Data saved successfully to {output_file}.")
     except IOError as e:
@@ -30,17 +30,18 @@ def save_publications(publications, output_file):
 def main():
     url = "https://croris.hr/crosbi-api/publikacija/?pageSize=100"
     output_file = "publications.pkl"
+    batch_size = 1000  # Process and save publications in batches of 1000
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3"
     }
 
-    all_publications = []
-
     try:
         with tqdm(desc="Fetching publications", unit="page") as pbar:
             page_number = 1
             retry_count = 0
+            batch_publications = []
+
             while True:
                 page_url = f"{url}&pageNumber={page_number}"
                 publications, status_code = fetch_publications(page_url, headers)
@@ -57,22 +58,29 @@ def main():
                         logging.error("Max retries reached. Stopping the script.")
                         break
 
-                all_publications.extend(publications)
+                batch_publications.extend(publications)
                 page_number += 1
                 pbar.update(1)
                 retry_count = 0
 
                 logging.info(f"Fetched page {page_number - 1} with status code {status_code}")
 
+                if len(batch_publications) >= batch_size:
+                    save_publications(batch_publications, output_file)
+                    batch_publications = []
+
+                if len(publications) < 100:
+                    # Break the loop if the current page has less than 100 publications
+                    break
+
                 time.sleep(random.randint(1, 4))  # Add a configurable delay between API requests
+
+            # Save any remaining publications in the batch
+            if batch_publications:
+                save_publications(batch_publications, output_file)
 
     except KeyboardInterrupt:
         logging.info("Program interrupted by the user.")
-
-    if all_publications:
-        save_publications(all_publications, output_file)
-    else:
-        logging.info("No publications found.")
 
 if __name__ == "__main__":
     main()
